128819fc7223401549f7db60c38223ef61fda978
mrkumar82 <mrkumar82@gmail.com>
2021-12-04 12:37:22 +0530

    updated mutation

--- src/components/post/Posts.tsx
+++ src/components/post/Posts.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import { usePostsQuery } from '../../services/api';
+import {
+  usePostsQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} from '../../services/api';
 import { Link } from 'react-router-dom';
-
+import { Post } from '../../models/post.model';
 const Posts = () => {
   const { data, error, isLoading, isSuccess } = usePostsQuery();
+  const [addPost] = useAddPostMutation();
+  const [updatePost] = useUpdatePostMutation();
+  const [deletePost] = useDeletePostMutation();
+
+  const post = {
+    id: '44',
+    title: 'Custom title',
+    body: 'Custom body',
+  };
+  const updatepost = {
+    id: '95',
+    title: 'Custom title Updated',
+    body: 'Custom body Updated',
+  };
+
+  const addPostHandler = async () => {
+    await addPost(post);
+  };
+  const updatePostHandler = async () => {
+    await updatePost(updatepost);
+  };
+  const deletePostHandler = async () => {
+    await deletePost('50');
+  };
+
   return (
     <div>
+      <button onClick={addPostHandler}>Add Post</button>
+      <button onClick={updatePostHandler}>Update Post</button>
+      <button onClick={deletePostHandler}>Delete Post</button>
       {isLoading && <h2>Loading...</h2>}
       {error && <h2>Something went wrong</h2>}
 
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -6,14 +6,46 @@ export const postsApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://jsonplaceholder.typicode.com/',
   }),
+  tagTypes: ['Post'],
   endpoints: (builder) => ({
     posts: builder.query<Post[], void>({
       query: () => '/posts',
+      providesTags: ['Post'],
     }),
     post: builder.query<Post, string>({
       query: (id) => `/posts/${id}`,
+      providesTags: ['Post'],
+    }),
+    addPost: builder.mutation<{}, Post>({
+      query: (post) => ({
+        url: `/posts`,
+        method: 'POST',
+        body: post,
+      }),
+      invalidatesTags: ['Post'],
+    }),
+    updatePost: builder.mutation<{}, Post>({
+      query: ({ id, ...rest }) => ({
+        url: `/posts/${id}`,
+        method: 'PUT',
+        body: rest,
+      }),
+      invalidatesTags: ['Post'],
+    }),
+    deletePost: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Post'],
     }),
   }),
 });
 
-export const { usePostsQuery, usePostQuery } = postsApi;
+export const {
+  usePostsQuery,
+  usePostQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postsApi;
